b8adf33f3359c51082325c758ebf525d787ec69c
Ovioisa Oisamaye Benjamin <99197115+Oisamaye1@users.noreply.github.com>
2022-12-25 19:09:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const inputEl = document.getElementById('input-el')
 const saveLead = document.getElementById('input-btn')
+let clearLead = document.getElementById('clear-btn')
 // const container = document.getElementById('container')
 const ulEL = document.getElementById('lead-list')
 let myLeads = []
+
+
+const localStorageLead = JSON.parse(localStorage.getItem('myLeads'))
+
+if (localStorageLead){
+    myLeads = localStorageLead
+    renderLeads()
+}
 
 function renderLeads(){
     let listItem = ''
@@ -14,15 +23,26 @@ function renderLeads(){
             </a>
         </li>`
 }
-
 ulEL.innerHTML = listItem
+
+
 }
 
+
+clearLead.addEventListener('click', function(){
+    console.log("helloo")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 saveLead.addEventListener('click', function(){
     myLeads.push(inputEl.value)
     if (inputEl != ''){
         inputEl.value = ''
     }
+
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
     renderLeads()
 })
+
